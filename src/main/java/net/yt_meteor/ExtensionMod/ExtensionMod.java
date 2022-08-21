4cd3af6113518a2b3df2bff175c0ad30749e1b15
MeteorYT <didiytchannel@gmail.com>
2022-08-21 16:24:20 +1000

    Added Blocks to the game!

--- src/main/java/net/yt_meteor/ExtensionMod/ExtensionMod.java
+++ src/main/java/net/yt_meteor/ExtensionMod/ExtensionMod.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.yt_meteor.ExtensionMod.block.ModBlocks;
 import net.yt_meteor.ExtensionMod.item.ModItems;
 import org.slf4j.Logger;
 
@@ -22,6 +23,7 @@ public class ExtensionMod {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
